86e721addbe1325275ee12788c24ab6081c61a16
10gic <juhani@163.com>
2015-05-08 20:52:52 +0800

    add function copy-buffer-file-name-as-kill

--- init.el
+++ init.el
@@ -602,3 +602,29 @@
   "Moves current line N (1) lines down leaving point in place."
   (interactive "p")
   (move-line (if (null n) 1 n)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Copy full file path into clipboard
+;;
+;; http://stackoverflow.com/questions/18812938/copy-full-file-path-into-copy-paste-clipboard
+;; If you are in dired mode already, you don't need it. w will copy the file
+;; name, c-u w will copy the relative file name and c-u 0 w will copy the
+;; absolute file name
+(defun copy-buffer-file-name-as-kill (choice)
+  "Copy the buffer-file-name to the kill-ring"
+  (interactive "cCopy Buffer Name (F) Full, (D) Directory, (N) Name")
+  (let ((new-kill-string)
+        (name (if (eq major-mode 'dired-mode)
+                  (dired-get-filename)
+                (or (buffer-file-name) ""))))
+    (cond ((eq choice ?f)
+           (setq new-kill-string name))
+          ((eq choice ?d)
+           (setq new-kill-string (file-name-directory name)))
+          ((eq choice ?n)
+           (setq new-kill-string (file-name-nondirectory name)))
+          (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string))))
