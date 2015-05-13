e4b8e01c8a9bc8bd6331dc79f4bb8837594607ac
10gic <juhani@163.com>
2015-05-13 17:01:15 +0800

    Create customize-html.el

---
+++ customize-html.el
@@ -0,0 +1,16 @@
+(require 'web-mode)
+;; web-mode.el is an autonomous emacs major-mode for editing web templates.
+;; http://web-mode.org/
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; Highlight current HTML element (highlight matched tags).
+(setq web-mode-enable-current-element-highlight t)
