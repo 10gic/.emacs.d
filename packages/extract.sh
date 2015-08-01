2bde716580a7802891bc726b895e6f9eb7152d03
10gic <juhani@163.com>
2015-08-01 08:45:36 +0800

    suppress some extracting output log

--- packages/extract.sh
+++ packages/extract.sh
@@ -13,14 +13,17 @@ if [ ! -d $extract_dir ]; then
     mkdir -p $extract_dir;
 
     for zip_file in `ls $current_dir/*.zip`; do
-        unzip $zip_file -d $extract_dir;
+        echo "Extracting $zip_file"
+        unzip $zip_file -d $extract_dir >/dev/null;
     done
 
     for gz_file in `ls $current_dir/*.gz`; do
-        tar -xzf $gz_file -C $extract_dir;
+        echo "Extracting $gz_file"
+        tar -xzf $gz_file -C $extract_dir >/dev/null;
     done
 
     for bz2_file in `ls $current_dir/*.bz2`; do
-        tar -xjf $bz2_file -C $extract_dir;
+        echo "Extracting $bz2_file"
+        tar -xjf $bz2_file -C $extract_dir >/dev/null;
     done
 fi
