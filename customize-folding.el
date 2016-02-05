4723d26ecf838ca113a45ac82fce3aa212179ffb
10gic <juhani@163.com>
2016-02-06 00:16:48 +0800

    reorganize code

--- customize-folding.el
+++
@@ -1,5 +0,0 @@
-(folding-add-to-marks-list 'my-refine-mode "#||"  "||#" nil t)
-(folding-add-to-marks-list 'refine-mode "#||"  "||#" nil t)
-
-;; (add-hook 'my-refine-mode-hook 'turn-on-folding-mode) ;不默认打开，折叠后行号与实际不匹配。
-;; (add-hook 'refine-mode-hook 'turn-on-folding-mode)
