d8ae91204c07477ec151b8fa98388688be7c1423
10gic <juhani@163.com>
2018-01-16 17:51:13 +0800

    Fix issues reported by shellcheck

--- packages/extract.sh
+++ packages/extract.sh
@@ -9,21 +9,21 @@ if [ $? -ne 0 ]; then
    echo "WARN: unzip is NOT found in PATH."
 fi
 
-if [ ! -d $extract_dir ]; then
-    mkdir -p $extract_dir;
+if [ ! -d "$extract_dir" ]; then
+    mkdir -p "$extract_dir";
 
-    for zip_file in `ls $current_dir/*.zip`; do
+    for zip_file in "$current_dir"/*.zip; do
         echo "Extracting $zip_file"
-        unzip $zip_file -d $extract_dir >/dev/null;
+        unzip "$zip_file" -d "$extract_dir" >/dev/null;
     done
 
-    for gz_file in `ls $current_dir/*.gz`; do
+    for gz_file in "$current_dir"/*.gz; do
         echo "Extracting $gz_file"
-        tar -xzf $gz_file -C $extract_dir >/dev/null;
+        tar -xzf "$gz_file" -C "$extract_dir" >/dev/null;
     done
 
-    for bz2_file in `ls $current_dir/*.bz2`; do
+    for bz2_file in "$current_dir"/*.bz2; do
         echo "Extracting $bz2_file"
-        tar -xjf $bz2_file -C $extract_dir >/dev/null;
+        tar -xjf "$bz2_file" -C "$extract_dir" >/dev/null;
     done
 fi
