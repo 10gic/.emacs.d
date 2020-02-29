2eaa596cc39602038c0fefa0f6c258b30ed6dece
10gic <juhani@163.com>
2020-02-29 14:29:18 +0800

    Comment out url-proxy-services

--- proxy.el
+++ proxy.el
@@ -1,5 +1,5 @@
 ;; Comment out it if you don't need proxy
-(setq url-proxy-services
-   '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
-     ("http" . "localhost:1087")
-     ("https" . "localhost:1087")))
+;; (setq url-proxy-services
+;;    '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+;;      ("http" . "127.0.0.1:1086")
+;;      ("https" . "127.0.0.1:1086")))
